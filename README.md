baab8b38bc6af11b76a508ae3a3188b63a7bad0d
Maciej.Iskra <Maciej.Iskra@ericpol.com>
2017-01-08 19:03:13 +0100

    Added usage sample and description

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# milight-v6-api
-Java API for milight/limitless bridge (bridge API v6)
+## API for milight iBox WIFI controller (Limitless Led API v6)
+Library provides Java API for milight/limitless WIFI bridge  of version 6 (Milight iBox)
+
+### Usage
+```java
+import com.jeefix.iot.milight.CommandService;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        CommandService commandService = new CommandService("172.23.4.225", 0); //set bridge IP, zone number
+        commandService.newMilightFlow()
+                .on() //turn lights on
+                .brightnessLevel(100) //set brightness to 100%
+                .delay(1000) //wait 1 second
+                .brightnessLevel(50) //set brightness to 50%
+                .delay(500)
+                .brightnessLevel(25)
+                .delay(500)
+                .off(); //turn lights off
+    }
+}
+```
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,17 @@
 import com.jeefix.iot.milight.CommandService;
 
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-
 public class Main {
-    private static DatagramSocket clientSocket;
-    private static InetAddress IPAddress;
-
-
 
     public static void main(String[] args) throws Exception {
-
-        CommandService commandService = new CommandService("172.23.4.225", 0);
-
-       commandService.newMilightFlow().on().brightnessLevel(64).delay(1000).brightnessLevel(32).delay(500).brightnessLevel(16).delay(500).off();
-
-
-
+        CommandService commandService = new CommandService("172.23.4.225", 0); //set bridge IP, zone number
+        commandService.newMilightFlow()
+                .on() //turn lights on
+                .brightnessLevel(100) //set brightness to 100%
+                .delay(1000) //wait 1 second
+                .brightnessLevel(50) //set brightness to 50%
+                .delay(500)
+                .brightnessLevel(25)
+                .delay(500)
+                .off(); //turn lights off
     }
-
-
-
-
 }
