a0b58b08bc48ca91991e4655a3af752944430f87
emacisk <Maciej.Iskra@ericsson.com>
2017-04-12 16:27:51 +0200

    Added non-tested implementation of API added state handler added shared session handler

--- src/main/java/SampleUsage.java
+++ src/main/java/SampleUsage.java
@@ -1,25 +1,16 @@
-import com.jeefix.iot.milight.CommandService;
-import com.jeefix.iot.milight.api.FluentCommandWrapper;
+import com.jeefix.limitlessled.device.LedStripDevice;
 
 public class SampleUsage {
 
     public static void main(String[] args) throws Exception {
-        CommandService commandService = new CommandService("172.23.4.225", 0); //set bridge IP, zone number
+        LedStripDevice commandService = new LedStripDevice("172.23.4.225", 0); //set bridge IP, zone number
 
-
-
-        commandService.newMilightFlow()
+        commandService
                 .on() //turn lights on
                 .hue(0) //set hue to 0 (near red color)
-                .saturationLevel(0) //set saturation level to 0%
-                .brightnessLevel(100) //set brightness to 100%
-                .delay(1000) //wait 1 second
-                .brightnessLevel(50) //set brightness to 50%
-                .delay(500)
-                .brightnessLevel(25)
-                .delay(500)
+                .saturation(0) //set saturation level to 0%
+                .brightness(100) //set brightness to 100%
                 .whiteOn() //turn on white led
-                .delay(500)
                 .off();
     }
 }
